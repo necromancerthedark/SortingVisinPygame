02f3f6ead3c5fa9c38fda3f32f18e22e95ba4fcb
Kumar Gaurav Pandey <kg22584@gmail.com>
2019-11-10 19:16:20 +0530

    Finally Completed! Visualisation Works Flawlessly

--- sortviz.py
+++ sortviz.py
@@ -4,6 +4,7 @@ white=(255,255,255)
 black =(0,0,0)
 pygame.init()
 values = []
+clock = pygame.time.Clock()
 
 window = pygame.display.set_mode((800,500))
 window.fill(black)
@@ -14,14 +15,18 @@ for i in range(780):
     values.append(Y)
 
 print(len(values))
+temp = 0
 
 # for i in values:
 #     if(X<780):
 #         pygame.draw.line(window,white,[X,20],[X,i])
 #         X+=1
 flag =0
+k=0
+j=0
 while not gameexit:
     X=20
+    window.fill(black)
     for i in values:
         if(X<780):
             pygame.draw.line(window,white,[X,20],[X,i])
@@ -29,13 +34,29 @@ while not gameexit:
             # pygame.display.update()
         flag+=1
     
+    clock.tick(30)
+    
     pygame.display.update()
-    for k in range(len(values)):
+    # for k in range(len(values)):
+    #     for j in range(len(values)-k-1):
+    #         if values[j]>values[j+1]:
+    #             values[j],values[j+1] = values[j+1],values[j]
+
+    if k<len(values):
         for j in range(len(values)-k-1):
             if values[j]>values[j+1]:
                 values[j],values[j+1] = values[j+1],values[j]
+        
+    # j=j+1
+    # temp = k
+    if j<len(values)-k-1:
+        k+=1
+        #j=0
+    # if temp+1 == k:
+    #     j=0
 
-    window.fill(black)
+
+   # window.fill(black)
     # for i in values:
     #     if(X<780):
     #         print(i)
